3e82a4ee8b747383b6bbb407030da1a5c5253de4
Alexander Bobykin <birmudo@gmail.com>
2023-05-30 13:52:29 +0600

    Adding Vue

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,25 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+
+
+  Learn to code by watching others
+
+  See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
+  but understanding how developers think is invaluable. 
+
+  Try it free 7 days then $20/mo. thereafter
+
+  First Name 
+  Last Name 
+  Email Address
+  Password
+
+  Claim your free trial 
+
+  By clicking the button, you are agreeing to our Terms and Services
+  
+
     </div>
   </header>
 
